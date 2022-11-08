32e5ed05fb3b146f758006c948665e5ba5892b83
Amol Vyas <54383954+loma373@users.noreply.github.com>
2022-11-08 12:58:44 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,8 @@
-
+import random
 
 def display_board(board):
-    
-    
     print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
     print('-----------')
     print('   |   |')
@@ -12,15 +10,13 @@ def display_board(board):
     print('   |   |')
     print('-----------')
     print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
 
 def player_input():
     marker = ''
-    
     while not (marker == 'X' or marker == 'O'):
         marker = input('Player 1: Do you want to be X or O? ').upper()
-
     if marker == 'X':
         return ('X', 'O')
     else:
@@ -30,17 +26,15 @@ def place_marker(board, marker, position):
     board[position] = marker
 
 def win_check(board,mark):
-    
-    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
-    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
-    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
-    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
-    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
-    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
-    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
-    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or 
+    (board[4] == mark and board[5] == mark and board[6] == mark) or 
+    (board[1] == mark and board[2] == mark and board[3] == mark) or 
+    (board[7] == mark and board[4] == mark and board[1] == mark) or
+    (board[8] == mark and board[5] == mark and board[2] == mark) or
+    (board[9] == mark and board[6] == mark and board[3] == mark) or 
+    (board[7] == mark and board[5] == mark and board[3] == mark) or 
+    (board[9] == mark and board[5] == mark and board[1] == mark)) 
 
-import random
 
 def choose_first():
     if random.randint(0, 1) == 0:
@@ -49,7 +43,6 @@ def choose_first():
         return 'Player 1'
 
 def space_check(board, position):
-    
     return board[position] == ' '
 
 def full_board_check(board):
@@ -60,20 +53,17 @@ def full_board_check(board):
 
 def player_choice(board):
     position = 0
-    
     while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
         position = int(input('Choose your next position: (1-9) '))
         
     return position
 
 def replay():
-    
     return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
 
 print('Welcome to Tic Tac Toe!')
 
 while True:
-    # Reset the board
     theBoard = [' '] * 10
     player1_marker, player2_marker = player_input()
     turn = choose_first()
@@ -89,7 +79,7 @@ while True:
     while game_on:
         if turn == 'Player 1':
             # Player1's turn.
-            
+            print("Player 1 your turn.")
             display_board(theBoard)
             position = player_choice(theBoard)
             place_marker(theBoard, player1_marker, position)
@@ -107,8 +97,7 @@ while True:
                     turn = 'Player 2'
 
         else:
-            # Player2's turn.
-            
+            print("Player 2 your turn.")
             display_board(theBoard)
             position = player_choice(theBoard)
             place_marker(theBoard, player2_marker, position)
